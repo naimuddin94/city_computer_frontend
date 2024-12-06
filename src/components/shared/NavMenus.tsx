c109e0dca5da513b0206ac27391cb9b4009bb9dc
naimuddin94 <developernaimuddin@gmail.com>
2024-12-06 13:05:40 +0600

    font setup

---
+++ src/components/shared/NavMenus.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { adminNavItems, navItem, userNavItems } from "@/utils/navItems";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+import { ListItem } from "./ListItem";
+
+const user = {
+  role: "admin",
+};
+
+const NavMenus = () => {
+  const pathname = usePathname();
+  const [menus, setMenus] = useState<navItem[]>(userNavItems);
+  useEffect(() => {
+    if (user?.role === "admin") {
+      setMenus(adminNavItems);
+    } else {
+      setMenus(userNavItems);
+    }
+  }, []);
+  return (
+    <ul className="items-center gap-5 py-3">
+      <NavigationMenu>
+        <NavigationMenuItem className="flex flex-col lg:flex-row">
+          {menus?.map((menu) => {
+            if (menu.href) {
+              return (
+                <NavigationMenuLink
+                  key={menu.href}
+                  className={`${navigationMenuTriggerStyle()} ${
+                    pathname === menu.href
+                      ? "border border-theme/40 bg-transparent"
+                      : "bg-transparent"
+                  }`}
+                >
+                  {menu.label}
+                </NavigationMenuLink>
+              );
+            } else if (menu.children) {
+              return (
+                <React.Fragment key={menu.label}>
+                  <NavigationMenuTrigger
+                    className={
+                      pathname.includes("dashboard")
+                        ? "border border-theme/40 bg-transparent"
+                        : "bg-transparent"
+                    }
+                  >
+                    Dashboard
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                      {menu.children?.map((component) => (
+                        <ListItem
+                          key={component.title}
+                          title={component.title}
+                          href={component.href}
+                        >
+                          {component.description}
+                        </ListItem>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </React.Fragment>
+              );
+            }
+          })}
+        </NavigationMenuItem>
+      </NavigationMenu>
+    </ul>
+  );
+};
+
+export default NavMenus;
